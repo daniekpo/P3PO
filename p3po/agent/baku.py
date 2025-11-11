Optional quaternions, action spec fix. Some sign of life

p3po/agent/baku.py
@@ -1,3 +1,4 @@
+import pickle
 import einops
 import numpy as np
 from collections import deque
@@ -411,6 +412,8 @@ class BCAgent:
         self.train()
         self.buffer_reset()
 
+        self.act_history = []
+
     def __repr__(self):
         return "bc"
 
@@ -519,20 +522,35 @@ class BCAgent:
             self.actor.parameters(), lr=self.lr, weight_decay=1e-4
         )
 
+    def preprocess(self, pose, norm_stats):
+        norm_min =norm_stats[self.proprio_key]['min']
+        norm_max = norm_stats[self.proprio_key]["max"]
+
+        new_pose = (pose - norm_min) / (norm_max - norm_min + 1e-5)
+        return new_pose
+
+    def postprocess(self, action, norm_stats):
+        actions_max = norm_stats["actions"]["max"]
+        actions_min = norm_stats["actions"]["min"]
+
+        action = action * (actions_max - actions_min) + actions_min
+        return action
+
+
     def act(self, obs, prompt, norm_stats, step, global_step, eval_mode=False):
-        if norm_stats is not None:
-            pre_process = lambda s_qpos: (
-                s_qpos - norm_stats[self.proprio_key]["min"]
-            ) / (
-                norm_stats[self.proprio_key]["max"]
-                - norm_stats[self.proprio_key]["min"]
-                + 1e-5
-            )
-            post_process = (
-                lambda a: a
-                * (norm_stats["actions"]["max"] - norm_stats["actions"]["min"])
-                + norm_stats["actions"]["min"]
-            )
+        # if norm_stats is not None:
+        #     pre_process = lambda s_qpos: (
+        #         s_qpos - norm_stats[self.proprio_key]["min"]
+        #     ) / (
+        #         norm_stats[self.proprio_key]["max"]
+        #         - norm_stats[self.proprio_key]["min"]
+        #         + 1e-5
+        #     )
+        #     post_process = (
+        #         lambda a: a
+        #         * (norm_stats["actions"]["max"] - norm_stats["actions"]["min"])
+        #         + norm_stats["actions"]["min"]
+        #     )
 
         # lang projection
         if self.use_language:
@@ -573,7 +591,7 @@ class BCAgent:
             )
             features.append(encoder_output)
         if self.use_proprio:
-            obs[self.proprio_key] = pre_process(obs[self.proprio_key])
+            obs[self.proprio_key] = self.preprocess(obs[self.proprio_key], norm_stats)
             self.proprio_buffer.append(obs[self.proprio_key])
             proprio = torch.as_tensor(
                 np.array(self.proprio_buffer), device=self.device
@@ -660,11 +678,19 @@ class BCAgent:
             exp_weights = torch.from_numpy(exp_weights).to(self.device).unsqueeze(dim=1)
             action = (actions_for_curr_step * exp_weights).sum(dim=0, keepdim=True)
             if norm_stats is not None:
-                return post_process(action.cpu().numpy()[0])
+                post_action = self.postprocess(action.cpu().numpy()[0], norm_stats)
+                self.act_history.append({"obs": obs, "action": action, "post_processed_action": post_action})
+                with open("eval_history.pkl", "wb") as f:
+                    pickle.dump(self.act_history, f)
+                return post_action
             return action.cpu().numpy()[0]
         else:
             if norm_stats is not None:
-                return post_process(action.cpu().numpy()[0, -1])
+                post_action = self.postprocess(action.cpu().numpy()[0, -1], norm_stats)
+                self.act_history.append({"obs": obs, "action": action, "post_processed_action": post_action})
+                with open("eval_history.pkl", "wb") as f:
+                    pickle.dump(self.act_history, f)
+                return post_action
             return action.cpu().numpy()[0, -1, :]
 
     def update(self, expert_replay_iter, step, update=True):
@@ -932,3 +958,4 @@ class BCAgent:
             for k in opt_keys:
                 self.__dict__[k] = payload[k]
         self.train(True)
+

p3po/eval.py
@@ -2,6 +2,7 @@
 
 import warnings
 import os
+import pickle
 
 os.environ["MKL_SERVICE_FORCE_INTEL"] = "1"
 os.environ["MUJOCO_GL"] = "egl"
@@ -97,8 +98,29 @@ class WorkspaceIL:
                 )
 
         # create agent
+        if self.cfg.use_training_action_spec:
+            with open(f"{self.cfg.training_dir}/action_spec.pkl", "rb") as f:
+                print("Using training action spec")
+                action_spec = pickle.load(f)
+        elif self.cfg.use_dataset_stats_for_action_spec:
+            print("Using dataset stats for action specs")
+            from dm_env import specs
+
+            action_spec = specs.BoundedArray(
+                (self.expert_replay_loader.dataset._max_action_dim,),
+                np.float32,
+                self.expert_replay_loader.dataset.stats["actions"]["min"],
+                self.expert_replay_loader.dataset.stats["actions"]["max"],
+                "action",
+            )
+        elif self.cfg.action_spec_path is not None:
+            with open(self.cfg.action_spec_path, "rb") as f:
+                action_spec = pickle.load(f)
+        else:
+            action_spec = self.env[0].action_spec()
+
         self.agent = make_agent(
-            self.env[0].observation_spec(), self.env[0].action_spec(), cfg
+            self.env[0].observation_spec(), action_spec, cfg
         )
 
         self.envs_till_idx = len(self.env)

p3po/read_data/p3po_xarm.py
@@ -87,6 +87,7 @@ class BCDataset(IterableDataset):
         subsample,
         skip_first_n,
         relative_actions,
+        use_quaternion_orientation,
     ):
         self._obs_type = obs_type
         self._prompt = prompt
@@ -180,9 +181,12 @@ class BCDataset(IterableDataset):
                 else:
                     actions = actions[self._action_after_steps :]
                 # Convert cartesian states to quaternion orientation
-                observations[i]["cartesian_states"] = get_quaternion_orientation(
-                    observations[i]["cartesian_states"]
-                )
+                if use_quaternion_orientation:
+                    observations[i]["cartesian_states"] = get_quaternion_orientation(
+                        observations[i]["cartesian_states"]
+                    )
+
+
                 # Repeat last dimension of each observation for history_len times
                 for key in observations[i].keys():
                     if key == "demo_dir":
@@ -226,6 +230,7 @@ class BCDataset(IterableDataset):
                     ),
                 )
                 self._max_state_dim = 7
+                self._max_action_dim = 7
                 self._num_samples += len(observations[i][self._keys[0]])
 
                 # max, min action
@@ -243,12 +248,17 @@ class BCDataset(IterableDataset):
             # keep record of max and min stat
             max_cartesian = data["max_cartesian"]
             min_cartesian = data["min_cartesian"]
-            max_cartesian = np.concatenate(
-                [data["max_cartesian"][:3], [1] * 4]
-            )  # for quaternion
-            min_cartesian = np.concatenate(
-                [data["min_cartesian"][:3], [-1] * 4]
-            )  # for quaternion
+
+            if use_quaternion_orientation:
+                max_cartesian = np.concatenate(
+                    [data["max_cartesian"][:3], [1] * 4]
+                )  # for quaternion
+                min_cartesian = np.concatenate(
+                    [data["min_cartesian"][:3], [-1] * 4]
+                )  # for quaternion
+            else:
+                max_cartesian = data["max_cartesian"]
+                min_cartesian = data["min_cartesian"]
             max_gripper = data["max_gripper"]
             min_gripper = data["min_gripper"]
             max_val = np.concatenate([max_cartesian, max_gripper[None]], axis=0)

p3po/suite/ur5e_env.py
@@ -121,9 +121,9 @@ class UR5Env(gym.Env):
         # Use conservative bounds for translation/rotation, gripper in [0, 1]
         action_low = np.array(
             [
-                -1.0,
-                -1.0,
-                0.0,  # x, y, z (meters)
+                -0.296657,
+                -0.80490048532146,
+                0.23781020226938232,  # x, y, z (meters)
                 -np.pi,
                 -np.pi,
                 -np.pi,  # rx, ry, rz (radians)
@@ -132,7 +132,15 @@ class UR5Env(gym.Env):
             dtype=np.float32,
         )
         action_high = np.array(
-            [1.0, 1.0, 1.0, np.pi, np.pi, np.pi, 1.0], dtype=np.float32
+            [
+                0.49383734924956946,
+                -0.20859620805567852,
+                0.6900017827785798,
+                np.pi,
+                np.pi,
+                np.pi,
+                1.0 # gripper
+            ], dtype=np.float32
         )
         self.action_space = spaces.Box(
             low=action_low, high=action_high, dtype=np.float32
@@ -147,8 +155,7 @@ class UR5Env(gym.Env):
                 }
             ),
             "joints": spaces.Box(-np.pi, np.pi, shape=(6,), dtype=np.float32),
-            "pose": spaces.Box(-np.inf, np.inf, shape=(6,), dtype=np.float32),
-            "features": spaces.Box(-np.inf, np.inf, shape=(6,), dtype=np.float32),
+            "pose": spaces.Box(action_low[:6], action_high[:6], shape=(6,), dtype=np.float32)
         }
 
         if self.include_depth: